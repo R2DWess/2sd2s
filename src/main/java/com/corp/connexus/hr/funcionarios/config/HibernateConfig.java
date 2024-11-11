ad9b399897fb41651fc418afcb76b389b2c3802b
R2DWess <gusterlima@hotmail.com>
2024-11-11 02:52:05 -0300

    chore: remove unused classes with no impact on application

---
+++ src/main/java/com/corp/connexus/hr/funcionarios/config/HibernateConfig.java
@@ -0,0 +1,17 @@
+//package com.corp.connexus.hr.funcionarios.config;
+//
+//import org.hibernate.id.IdentifierGenerator;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class HibernateConfig {
+//
+//    @Bean
+//    public IdentifierGenerator customFuncionalGenerator() {
+//        return new CustomFuncionalGenerator();
+//    }
+//
+//    // Caso seja necessário configurar uma fábrica de sessões do Hibernate:
+//    // Configure a SessionFactory, se necessário
+//}
