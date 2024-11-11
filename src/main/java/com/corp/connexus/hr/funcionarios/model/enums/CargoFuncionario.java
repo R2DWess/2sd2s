7663321b4b74c7e424e08d001c1ea1bb8f5dae75
R2DWess <gusterlima@hotmail.com>
2024-11-11 00:34:12 -0300

    feat: add enums for employee roles

---
+++ src/main/java/com/corp/connexus/hr/funcionarios/model/enums/CargoFuncionario.java
@@ -0,0 +1,13 @@
+package com.corp.connexus.hr.funcionarios.model.enums;
+
+public enum CargoFuncionario {
+    GERENTE,
+    DESENVOLVEDOR,
+    ANALISTA,
+    SUPORTE,
+    ADMINISTRATIVO,
+    FINANCEIRO,
+    RH,
+    MARKETING,
+    VENDAS
+}
